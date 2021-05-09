fc81e571b79e3e50d2e3e75a99f3c3a4d2073142
VJanoti <vini.janoti@gmail.com>
2021-05-09 15:08:39 -0300

    Update testzone.py

--- testzone.py
+++ testzone.py
@@ -26,3 +26,19 @@ grafico = strat.graph(ganhoestrat, buyhold, combined2)
 
 grafico.show()
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+ax.grid()
+
+plt.show()
+
